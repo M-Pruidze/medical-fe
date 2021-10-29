e8e7c294100f04310cbcb80b033fb1915a43b472
M-Pruidze <m.pruidze.exceedteam@gmail.com>
2021-10-29 16:44:46 +0400

    added error component

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import Login from './components/login/Login';
 import MedApp from './components/medApp/MedApp';
+import NotFound from './components/notFound/NotFound';
 
 ReactDOM.render(
   <BrowserRouter>
@@ -18,6 +19,7 @@ ReactDOM.render(
     <Route exact path='/visit'>
       <MedApp />
     </Route>
+    <Route path='*' component={NotFound} />
     </ Switch>
   </BrowserRouter>,
   document.getElementById('root')
